83941433bda0ca07b12b702c9d86b58f9f239c23
Agusiglesias123 <91150896+Agusiglesias123@users.noreply.github.com>
2022-06-26 18:56:59 -0300

    Add files via upload

--- Arcana27-04/main.js
+++ Arcana27-04/main.js
@@ -40,4 +40,18 @@ aire.addEventListener('click', () => {
     pasarColor('aire');
 });
 
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+
 
